4313a06231e3bfcdd479158ac1d3aab70ec39df6
KingCharlesVI <89871965+KingCharlesVI@users.noreply.github.com>
2024-05-20 18:32:10 +0100

    Update FileReport.js
    - Added success alert

--- cheaterwatch-ui/src/components/FileReport.js
+++ cheaterwatch-ui/src/components/FileReport.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Container, FormControl, InputLabel, OutlinedInput, Button, Typography, Select, MenuItem, Checkbox, ListItemText } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import axios from 'axios'; // Import Axios for making HTTP requests
+import { Snackbar } from '@mui/material';
 import CallOfDutyMWLogo from '../assets/mw3.png';
 import WarzoneLogo from '../assets/wz.png';
 import ValorantLogo from '../assets/valorant.png';
@@ -107,12 +108,30 @@ const CheatReportForm = () => {
     { value: 'Apex Legends', label: 'Apex Legends', logo: ApexLegendsLogo },
   ];
 
+  const [alertOpen, setAlertOpen] = useState(false);
+
+  const handleSubmitWithAlert = (e) => {
+    e.preventDefault();
+    // Handle form submission...
+    setAlertOpen(true); // Open the alert after successful submission
+  };
+
+  const handleCloseAlert = () => {
+    setAlertOpen(false); // Close the alert when dismissed by the user
+  };
+
   return (
     <StyledContainer maxWidth="sm">
+    <Snackbar
+      open={alertOpen}
+      autoHideDuration={6000} // Adjust as needed
+      onClose={handleCloseAlert}
+      message="Report submitted successfully!"
+    />
       <Typography variant="h4" gutterBottom>
         Cheat Report Form
       </Typography>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmitWithAlert}>
         <StyledFormControl fullWidth variant="outlined">
           <InputLabel htmlFor="game">Game</InputLabel>
           <Select
